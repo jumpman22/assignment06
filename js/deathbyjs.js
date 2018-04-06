69f1ef8fe9b5dba1806e7ab30c36157362c41a77
jumpman22 <hiram.cho@gmail.com>
2018-04-05 23:54:11 -0700

    finish

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -5,41 +5,59 @@
 //Sample Data and Output
 //Example string: 'webmaster' 
 //Expected Output: 'abeemrstw'
-function alphabetize(str) {
-    "use strict";
-    str.split("").sort().join("");
-}
-alphabetize("webmaster");
 
-//STEP 2
+//function alphabetize(str) {
+//    "use strict";
+//    var x = str.split("").sort().join("");
+//		window.console.log(x);
+//}
+//alphabetize("webmaster");
 
-//STEP 3
-
-//STEP 4
-
-//STEP 5
-
-
-
-//2.	Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string to upper case.
+//STEP 2 Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string to upper case.
 //
 //Sample Data and Output
 //Example string: 'the quick brown fox' 
 //Expected Output: 'The Quick Brown Fox'
-//
-//3.	Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+//function upCase(str) {
+//	var upper = String(str);
+//  var newStr = upper.replace(/(^| )(\w)/g, function(first) {
+//    return first.toUpperCase();
+//  });
+//	window.console.log(newStr);
+//}
+//upCase("the quick brown fox");
+
+
+//STEP 3 Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
 //
 //Sample Data and Output
 //Example string: 'The quick brown fox' 
 //Expected Output: 5
-//
-//4.	Write a JavaScript function that generates a string id (specified length) of random characters.
+//function vowels(str) {
+//	var count = str.match(/[aeiou]/gi);
+//	window.console.log("There are " + count.length + " vowels in the string:\n" + str);
+//}
+//vowels("The quick brown fox");
+
+//STEP 4 Write a JavaScript function that generates a string id (specified length) of random characters.
 //
 //Sample Data and Output
 //Expected Output: RCv1SBUC or PmJkc2Y2 or nIi5L0FA
-//
-//5.	Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
+//function random() {
+//	var x = Math.random().toString(36).substr(2, 8);
+//	window.console.log(x);
+//}
+//random();
+
+
+//STEP 5  Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
 //
 //Sample Data and Output
 //Sample function: Longest_Country_Name(["Australia", "Germany", "United States of America"])
 //Expected output: "United States of America"
+//function longest(arr) {
+//	var x = arr.sort(function (a, b) { return b.length - a.length; })[0];	
+//	window.console.log(x);
+//}
+//longest(["Australia", "Germany", "United States of America"]);
+
