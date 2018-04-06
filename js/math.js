97b35276b48570cb0710a3a1e581dc4504a451b6
jumpman22 <hiram.cho@gmail.com>
2018-04-05 19:50:39 -0700

    parts 1 and 2

--- js/math.js
+++ js/math.js
@@ -1,11 +1,32 @@
 /*eslint-env browser*/
 
-//STEP 1
+//STEP 1 Create an application that prompts the user for a number. Regardless of whether they enter a negative or positive number, make sure to display the positive version of that number in the console window. If I enter -15 in the prompt and -15 is displayed in the console window, you did this one wrong.
+//var number = window.prompt("Enter a postive or negative number");
+//window.alert(Math.abs(number));
 
-//STEP 2
+//STEP 2 Create an application that prompts the user for a floating point value (decimal). Store the result of that input in a variable and then round it up to the nearest whole number. Display the result within a console window.
+//var num = window.prompt("Enter a number with a decimal");
+//window.alert(Math.round(num));
 
-//STEP 3
+//STEP 3 Create an application that prompts the user for a floating point value (decimal). Store the result of that input in a variable and then round it down to the nearest whole number. Display the result within a console window.
+//var num = window.prompt("Enter a number with a decimal");
+//window.alert(Math.floor(num));
 
-//STEP 4
+//STEP 4 Create an application that prompts the user for 5 numbers. Ask them to comma delimit each number so you get 1,2,3,4,5 for example. Store the result of that input in a variable and then find the largest and smallest numbers in that list. Display both of those numbers within a console window.
+//var numbers = window.prompt("Enter 5 numbers separated with commas - ex. 1,2,3,4,5");
+//var new_numbers = numbers.split(",").map(Number);
+//
+//var max = new_numbers.reduce(function(a, b) {
+//    return Math.max(a, b);
+//});
+//
+//var min = new_numbers.reduce(function(a, b) {
+//    return Math.min(a, b);
+//});
+//
+//window.console.log("The largest number was " + max + "\nThe smallest number was " + min);
 
-//STEP 5
+//STEP 5 Create an application that prompts the user for a number. Now find the square root of that number and display the result within a console window. 
+//var num = window.prompt("Enter a number");
+//var sqrt = Math.sqrt(num);
+//window.console.log("The square of root " + num + " is " + sqrt);
--- js/strings.js
+++ js/strings.js
@@ -19,29 +19,35 @@
 //window.alert(text.indexOf("fox"));
 
 //STEP 5 Create an application that stores the text “The quick brown fox jumps over the lazy fox” within a variable. Then, find and display within an alert the index of the last instance of the word “fox”.
-var text = "The quick brown fox jumps over the lazy dog";
-window.alert(text.lastIndexOf("fox"));
+//var text = "The quick brown fox jumps over the lazy dog";
+//window.alert(text.lastIndexOf("fox"));
 
-//STEP 6
+//STEP 6 Create an application that stores the text “The quick brown fox jumped over the lazy dog” within a variable. Then, prompt the user for their full name. Then, replace the text “the lazy dog” in the variable with the name that the user enters within the prompt. Use the alert method to display the result.
+//var text = "The quick brown fox jumps over the lazy dog";
+//var name = window.prompt("What is your full name?");
+//window.alert(text.replace(" the lazy dog", " " + name));
 
-//STEP 7
+//STEP 7 Create an application that stores the text “The quick brown fox jumps over the lazy dog” within a variable. Then, prompt the user for a word. Next, search for the word within the string that the user enters into the prompt. Use the alert method to display the result.
+//var text = "The quick brown fox jumps over the lazy dog";
+//var word = window.prompt("Enter a word");
+//found = text.search(word);
+//if(found !== -1) {
+//    window.alert("Your word was found at position " + found);
+//} else {
+//    window.alert("Your word was not found.");
+//}
 
-//STEP 8
+//STEP 8 Create an application that stores the text “The quick brown fox jumps over the lazy dog” within a variable called old_string. Then, use slice(), substr(), or substring() to extract the words “the lazy dog” from the old_string variable and store that result in a second variable called new_string. Use the alert method to display the uppercase result of new_string.
+//var old_string = "The quick brown fox jumps over the lazy dog";
+//var new_string = old_string.slice(31);
+//window.alert(new_string);
 
-//STEP 9
+//STEP 9 Create an application that stores the text “            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ” within a variable. Make sure to add space before and after the text so that appears very similar to the text here. Use the alert method to display the lowercase result of the variable once the space has been trimmed off.
+//var text = "            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
+//text = text.trim().toLowerCase();
+//window.alert(text);
 
-//STEP 10
-
-
-
-
-//1.	
-//2.	
-//3.	
-//4.	
-//5.	
-//6.	Create an application that stores the text “The quick brown fox jumped over the lazy dog” within a variable. Then, prompt the user for their full name. Then, replace the text “the lazy dog” in the variable with the name that the user enters within the prompt. Use the alert method to display the result.
-//7.	Create an application that stores the text “The quick brown fox jumps over the lazy dog” within a variable. Then, prompt the user for a word. Next, search for the word within the string that the user enters into the prompt. Use the alert method to display the result.
-//8.	Create an application that stores the text “The quick brown fox jumps over the lazy dog” within a variable called old_string. Then, use slice(), substr(), or substring() to extract the words “the lazy dog” from the old_string variable and store that result in a second variable called new_string. Use the alert method to display the uppercase result of new_string.
-//9.	Create an application that stores the text “            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ” within a variable. Make sure to add space before and after the text so that appears very similar to the text here. Use the alert method to display the lowercase result of the variable once the space has been trimmed off.
-//10.	Create an application that stores the text “the quick brown fox jumps over the lazy dog” within a variable. The user clearly forgot to capitalize the first letter in the sentence. Programmatically capitalize the first letter in the sentence and display the result in an alert.
+//STEP 10 Create an application that stores the text “the quick brown fox jumps over the lazy dog” within a variable. The user clearly forgot to capitalize the first letter in the sentence. Programmatically capitalize the first letter in the sentence and display the result in an alert.
+//var text = "the quick brown fox jumps over the lazy dog";
+//text = text.charAt(0).toUpperCase() + text.slice(1);
+//window.alert(text);
