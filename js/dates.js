9d7ba9d5e0130e64f0c7f631d202bcc35f34f19c
jumpman22 <hiram.cho@gmail.com>
2018-04-05 21:31:52 -0700

    part 3

--- js/dates.js
+++ js/dates.js
@@ -1,11 +1,36 @@
 /*eslint-env browser*/
 
-//STEP 1
+//STEP 1 Create an application that gets the number of days in a month. Display that result within the console window.
+//var current_month = new Date().getMonth() + 1;
+//if(current_month === 1 || current_month === 3 || current_month === 5 || current_month ===7 || current_month === 8 || current_month === 10 || current_month === 12) {
+//  window.console.log("There are 31 days this month");  
+//} else if(current_month === 2 ) {
+//  window.console.log("There are 28 days this month");
+//} else {
+//  window.console.log("There are 30 days this month");
+//}
 
-//STEP 2
+//STEP 2 Create an application that gets the month name from a particular date. Display that result within the console window.
+//var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//var current_month = new Date().getMonth();
+//window.console.log("The current month is " + months[current_month]);
 
-//STEP 3
+//STEP 3 Create an application that tests whether a date is a weekend. Display that result within the console window.
+//var current_day = new Date().getDay();
+//if(current_day === 0 || current_day === 6) {
+//    window.console.log("Today is a weekend date");
+//} else {
+//    window.console.log("Today is not a weekend date");
+//}
 
-//STEP 4
+//STEP 4 Create an application that gets yesterday’s day of the week. For instance, if today is Tuesday, the console window should display Monday. 
+//var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//var yday = new Date().getDay() - 1;
+//window.console.log("Yesterday was " + days[yday]);
 
-//STEP 5
+//STEP 5 Create an application that gets the current day of the week. The twist here is that I want only the first letter of the day. If today is Tuesday, the letter T should be displayed in the console window.
+//var days = ["S", "M", "T", "W", "T", "F", "S"];
+//var current_day = new Date().getDay();
+//window.console.log(days[current_day]);
+
+
--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,6 +1,15 @@
 /*eslint-env browser*/
+//Solve the following string/math/number/function/array-related problems. Each solution should be written within its own function and the output should be //displayed within a console window. The display should be the return value of the function call.
 
-//STEP 1
+//STEP 1 Write a JavaScript function that returns a passed string with letters in alphabetical order. Assume punctuation and number symbols are not included in the passed-in string.
+//Sample Data and Output
+//Example string: 'webmaster' 
+//Expected Output: 'abeemrstw'
+function alphabetize(str) {
+    "use strict";
+    str.split("").sort().join("");
+}
+alphabetize("webmaster");
 
 //STEP 2
 
@@ -9,3 +18,28 @@
 //STEP 4
 
 //STEP 5
+
+
+
+//2.	Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string to upper case.
+//
+//Sample Data and Output
+//Example string: 'the quick brown fox' 
+//Expected Output: 'The Quick Brown Fox'
+//
+//3.	Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+//
+//Sample Data and Output
+//Example string: 'The quick brown fox' 
+//Expected Output: 5
+//
+//4.	Write a JavaScript function that generates a string id (specified length) of random characters.
+//
+//Sample Data and Output
+//Expected Output: RCv1SBUC or PmJkc2Y2 or nIi5L0FA
+//
+//5.	Write a JavaScript function that accepts a list of country names as input and returns the longest country name as output.
+//
+//Sample Data and Output
+//Sample function: Longest_Country_Name(["Australia", "Germany", "United States of America"])
+//Expected output: "United States of America"
